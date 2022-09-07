159e73fd0609399a2cea37fb1ea2d1a3aaa4bbb2
Keith <keithwesley187@gmail.com>
2022-09-07 21:52:17 +0300

    milestone: Added POST functionality

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
-  # GET REQUESTS
+  # GET endpoints
   get "/" do
     { message: "Good luck with your project!" }.to_json
   end
@@ -46,9 +46,24 @@ class ApplicationController < Sinatra::Base
     all_roles.to_json(only: [:id, :role_name, :image_url, :description])
   end
 
+  get "/roles/:id" do
+    all_roles = Role.find(params[:id])
+    all_roles.to_json(only: [:id, :role_name, :image_url, :description])
+  end
+
   get "/players" do
     all_players = Player.all
     all_players.to_json(only: [:id, :name, :age, :slogan, :role_played, :image_url])
+  end
+
+  get "/players/:id" do
+    all_players = Player.find(params[:id])
+    all_players.to_json(only: [:id, :name, :age, :slogan, :role_played, :image_url])
+  end
+
+  get "/players/:id/stats" do
+    all_players = Player.find(params[:id])
+    all_players.to_json(only: [:id, :name, :age], include: {player_stats: {only: [:id, :number_of_games, :game_duration, :average_kills_per_game, :favourite_role]}})
   end
 
   get "/players/stats" do
@@ -62,10 +77,76 @@ class ApplicationController < Sinatra::Base
     all_teams.to_json(only: [:id, :name, :team_owner, :founded, :description])
   end
 
+  get "/teams/:id" do
+    specific_team = Team.all
+    specific_team.to_json(only: [:id, :name, :team_owner, :founded, :description])
+  end
+
   get "/teams/players" do
     all_teams = Team.all
     all_teams.to_json(only: [:id, :name, :team_owner, :founded, :description], 
       include: {players: {only: [:id, :name, :age, :slogan, :role_played, :image_url]}})
+  end
+
+  get "/stats" do
+    all_stats = PlayerStat.all
+    all_stats.to_json(only: [:id, :number_of_games, :game_duration, :average_kills_per_game, :favourite_role], 
+      include: {player: {only: [:id, :name]}})
+  end
+
+  get "/stats/:id" do
+    all_stats = PlayerStat.find(params[:id])
+    all_stats.to_json(only: [:id, :number_of_games, :game_duration, :average_kills_per_game, :favourite_role], 
+      include: {player: {only: [:id, :name]}})
+  end
+
+  #POST endpoints
+
+  post "/teams" do
+    post_team = Team.create(
+      name: params[:name],
+      team_owner: params[:team_owner],
+      founded: params[:founded],
+      description: params[:description],
+      game_id: params[:game_id],
+      kenya_esport_league_id: params[:kenya_esport_league_id]
+    )
+    post_team.to_json
+  end
+
+  post "/players" do
+    post_player = Team.create(
+      name: params[:name],
+      age: params[:age],
+      slogan: params[:slogan],
+      role_played: params[:role_played],
+      image_url: params[:image_url],
+      team_id: params[:team_id]
+    )
+    post_player.to_json
+  end
+
+  post "/kenya_esport_league_table" do
+    post_team = KenyaEsportLeague.create(
+      league_name: params[:league_name],
+      played: params[:played],
+      won: params[:won],
+      lost: params[:lost],
+      drawn: params[:drawn],
+      points: params[:points]
+    )
+    post_team.to_json
+  end
+
+  post "/stats" do
+    send_stats = PlayerStat.create(
+      number_of_games: params[:number_of_games],
+      game_duration: params[:game_duration],
+      average_kills_per_game: params[:average_kills_per_game],
+      favourite_role: params[:favourite_role],
+      player_id: params[:player_id]
+    )
+    send_stats.to_json
   end
 
 end
